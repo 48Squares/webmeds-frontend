d0cbb1dcb208fe4875d1eae85c25b54a362d7064
Ganesh K <ganeshk.work@gmail.com>
2021-03-10 18:44:26 +0530

    Fox

--- pages/index.js
+++ pages/index.js
@@ -53,6 +53,19 @@ class App extends React.Component {
         return (
             <div>
                 <Head>
+                    <!-- Twitter Card data -->
+                    <meta name="twitter:card" content="product"/>
+                    <meta name="twitter:site" content="@publisher_handle"/>
+                    <meta name="twitter:title" content="Page Title"/>
+                    <meta name="twitter:description" content="Page description less than 200 characters"/>
+                    <meta name="twitter:creator" content="@author_handle"/>
+                    <meta name="twitter:image" content="http://www.example.com/image.jpg"/>
+                    <meta name="twitter:data1" content="$3"/>
+                    <meta name="twitter:label1" content="Price"/>
+                    <meta name="twitter:data2" content="Black"/>
+                    <meta name="twitter:label2" content="Color"/>
+                    <!-- Twitter Card data -->
+
                     <title>Webmeds</title>
                     <meta key="description"
                           content="Webmeds is multivendor medical platform allowing every medical vendor to sell medicine with peace of mind."/>
