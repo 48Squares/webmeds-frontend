cc72ae3efb692421e98c7c638367aabd18e16741
Ganesh K <ganeshk.work@gmail.com>
2021-03-17 15:36:55 +0530

    add back

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Header({title}) {
     return (
         <div className="bg-regal-blue">
             <div className="flex justify-center py-4">
-                <button type="button" className="ml-2 mr-3 focus:outline-none inline-block">
+                <Link href="/" className="ml-2 mr-3 focus:outline-none inline-block">
                     <svg className="w-6 h-6 text-white block" xmlns="http://www.w3.org/2000/svg" fill="none"
                          viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
                               d="M10 19l-7-7m0 0l7-7m-7 7h18"/>
                     </svg>
-                </button>
+                </Link>
 
                 <div className="text-white flex-grow">{title}</div>
             </div>
