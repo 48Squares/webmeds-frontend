14ee440a03cd98753a52ef7e5e767bcb7add0a68
mukesh856258 <81097887+mukesh856258@users.noreply.github.com>
2021-04-14 11:10:29 +0530

    link

--- components/SideNav.js
+++ components/SideNav.js
@@ -57,16 +57,18 @@ function SideNav(props) {
 
                 <hr className="my-2" />
 
-                <div className="flex items-center space-x-2 my-2">
-                    <svg className="w-6 h-6 text-regal-blue" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 24 24" stroke="currentColor">
-                        <path fill="#fff" d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z" />
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                            d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0" />
-                    </svg>
+                <Link href="/myorder">
+                    <div className="flex items-center space-x-2 my-2">
+                        <svg className="w-6 h-6 text-regal-blue" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path fill="#fff" d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                                d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0" />
+                        </svg>
 
-                    <span className="text-black text-sm font-medium">My Orders</span>
-                </div>
+                        <span className="text-black text-sm font-medium">My Orders</span>
+                    </div>
+                </Link>
 
                 <div className="flex items-center space-x-2 my-2">
                     <svg className="w-6 h-6 text-regal-blue" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -112,16 +114,17 @@ function SideNav(props) {
                     </Link>
                 </div>
 
-                <div className="flex items-center space-x-2 my-2">
-                    <svg className="w-6 h-6 text-regal-blue" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
+                <Link href="/notification">
+                    <div className="flex items-center space-x-2 my-2">
+                        <svg className="w-6 h-6 text-regal-blue" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
 
-                    <span className="text-black text-sm font-medium">My Notification</span>
-                </div>
-
+                        <span className="text-black text-sm font-medium">My Notification</span>
+                    </div>
+                </Link>
                 <hr className="my-2" />
 
                 <div className="flex items-center space-x-2 my-2">
