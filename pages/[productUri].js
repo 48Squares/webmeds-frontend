ed6abe1061906f479ac076075388194ce3068a54
Ganesh K <ganeshk.work@gmail.com>
2021-05-28 06:52:36 +0530

    fix

--- pages/[productUri].js
+++ pages/[productUri].js
@@ -1,37 +1,38 @@
-import {useEffect, useState} from "react";
+import React from "react";
 
-export default function Product() {
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-        fetch(
-            'http://mgmt.webmeds.in/api/products',
-            {
+export default class Product extends React.Component {
+    componentWillMount() {
+        fetch('http://mgmt.webmeds.in/api/products', {
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
                 }
+            }
+        )
+            .then(result => result.json())
+            .then(json => this.setState({products: json.data}))
+            .catch(error => {
+                console.log(error);
             })
-            .then(response => {
-                if (response.status >= 200 && response < 300) {
-                    return response;
-                } else {
-                    console.log(response);
-                    return new Error('Serve Error');
-                }
-            })
-            .then(response => response.json())
-            .then(json => setProducts(json.data.data))
-            .catch(() => []);
-    });
+    }
 
-    return (
-        <div className="px-4 py-2 bg-indigo-50">
-            <div className="flex justify-evenly">
-                {products.map((product, index) =>
-                    <img src={product.featuredImage} alt="" key={index} width="150"/>
-                )}
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: []
+        };
+    }
+
+    render() {
+        return(
+            <div className="px-4 py-2 bg-indigo-50">
+                <div className="flex justify-evenly">
+                    {this.state.products.map((product, index) =>
+                        <img src= {product.featuredImage} alt="" key={index} width="150"/>)
+                }
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
