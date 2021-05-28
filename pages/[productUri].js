7a3c5e19f232b92d527784831ccabbe3a5c0dc23
Ganesh K <ganeshk.work@gmail.com>
2021-05-28 07:48:52 +0530

    fix

--- pages/[productUri].js
+++ pages/[productUri].js
@@ -1,7 +1,11 @@
 import React from "react";
-import Product from '../components/Product';
 
 export default class ProductList extends React.Component {
-
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
 }
 
